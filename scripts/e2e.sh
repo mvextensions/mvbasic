2e7d8e95866998385b1f94cb906afe2b05d62eea
Mike Wright <mike.wright@itsallwater.net>
2020-12-23 11:16:51 -0700

    adding e2e tests!

--- scripts/e2e.sh
+++ scripts/e2e.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export CODE_TESTS_PATH="$(pwd)/client/out/test"
-export CODE_TESTS_WORKSPACE="$(pwd)/client/testFixture"
+export CODE_TESTS_PATH="./client/out/test"
+export CODE_TESTS_WORKSPACE="./client/testFixture"
 
-node "$(pwd)/client/out/test/runTest"
+node.exe "./client/out/test/runTest"
